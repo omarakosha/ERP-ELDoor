c656f491bee80731dae6b19910d684e11990b985
omarakosha <omerakosha@gmail.com>
2026-02-11 16:21:26 +0300

    last update add buildBreadcrumbl  11/2 5pm

--- src/app/layout/component/app.footer.ts
+++ src/app/layout/component/app.footer.ts
@@ -1,18 +1,38 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
     standalone: true,
     selector: 'app-footer',
+    imports: [
+        CommonModule,
+        TranslateModule   // ✅ هذا هو الحل
+    ],
     template: `
+<div class="layout-footer flex items-center justify-between px-12 py-3">
     
-    <div class="layout-footer flex items-center gap-2">
-    <span>Developed by</span>
-    <a href="https://Rqm.com" target="_blank" rel="noopener noreferrer" class="text-primary font-bold hover:underline flex items-center gap-2">
-        <img src="assets/icons/rqm.png" alt="Company Logo"  style="width: 35.8px; height: 35.8px;" class="object-contain"/>
-        Rqm-Ltd
-    </a>
-</div>
+    <span>
+        {{ 'footer.rights' | translate }}
+    </span>
 
+    <a href="https://Rqm.com"
+       target="_blank"
+       rel="noopener noreferrer"
+       class="text-primary font-bold hover:underline flex items-center gap-2">
+       
+       {{ 'footer.poweredBy' | translate }}
+        <img src="assets/icons/rqm.png"
+             alt="Company Logo"
+             style="width: 35.8px; height: 35.8px;"
+             class="object-contain"/>
+    </a>
+
+</div>
     `
 })
-export class AppFooter {}
+export class AppFooter {
+
+    constructor(private translate: TranslateService) { }
+
+}
